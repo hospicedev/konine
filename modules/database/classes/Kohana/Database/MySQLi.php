ed84490eafddefb62d2f3ac67c87412a7dd415cb
timhj <github@reticence.com.au>
2026-04-14 21:35:20 +0800

    fix: add MySQL JSON column type support to MySQLi driver
    Map the native MySQL JSON column type to PHP string with a max length of
    4294967295 (matching longtext). Add json to the collation_name switch so
    SHOW COLUMNS metadata is handled consistently with other text-based types.
    
    Without this, ORM::factory() on a model with a JSON column throws an
    unknown data type error, or falls through to unhandled type handling.
    
    Refs #4

--- modules/database/classes/Kohana/Database/MySQLi.php
+++ modules/database/classes/Kohana/Database/MySQLi.php
@@ -222,6 +222,7 @@ class Kohana_Database_MySQLi extends Database {
 			'integer unsigned'          => ['type' => 'int', 'min' => '0', 'max' => '4294967295'],
 			'longblob'                  => ['type' => 'string', 'binary' => TRUE, 'character_maximum_length' => '4294967295'],
 			'longtext'                  => ['type' => 'string', 'character_maximum_length' => '4294967295'],
+			'json'                      => ['type' => 'string', 'character_maximum_length' => '4294967295'],
 			'mediumblob'                => ['type' => 'string', 'binary' => TRUE, 'character_maximum_length' => '16777215'],
 			'mediumint'                 => ['type' => 'int', 'min' => '-8388608', 'max' => '8388607'],
 			'mediumint unsigned'        => ['type' => 'int', 'min' => '0', 'max' => '16777215'],
@@ -375,6 +376,7 @@ class Kohana_Database_MySQLi extends Database {
 						case 'char':
 						case 'varchar':
 							$column['character_maximum_length'] = $length;
+						case 'json':
 						case 'text':
 						case 'tinytext':
 						case 'mediumtext':
